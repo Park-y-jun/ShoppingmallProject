d7fe9ac2517a177a9fdc817ed86e2db5ecd11900
ParkYoungJun123 <aptl741@gmail.com>
2023-02-28 01:25:03 +0900

    jwt 방식 변경

--- src/App/middleware/auth.js
+++ src/App/middleware/auth.js
@@ -1,19 +1,15 @@
-// jwt 토큰을 디코드 하여서 거기에 맞는 유저정보 불러옴
-
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../../DB/models/user/User");
 
 const authToken = async (req, res, next) => {
-  const token = req.cookies.auth;
-
-  if (!token) {
-    res.status(400).json({
-      success: false,
-      msg: "토큰이 없습니다.",
-    });
-  }
   try {
+    const token = req.body.token;
+
+    if (!token) {
+      throw new Error("토큰이 없습니다.");
+    }
+
     const verifyUser = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({ email: verifyUser.email });
     req.user = user;
@@ -22,7 +18,7 @@ const authToken = async (req, res, next) => {
   } catch (err) {
     res.status(400).json({
       success: false,
-      msg: "토큰이 유효하지 않습니다.",
+      msg: err.message,
     });
   }
 };
