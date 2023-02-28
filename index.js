5571e85f08ed6347932d2b334b3ed3b35cf80f2b
ParkYoungJun123 <aptl741@gmail.com>
2023-03-01 01:41:30 +0900

    cors 설정

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:5000"],
     credentials: true,
   })
 );
